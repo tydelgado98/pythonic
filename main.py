ed3a6eebdc1e2ecb6b6faabec0b85f29a550a52b
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-03 16:43:59 -0400

    making a pygame

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
     pygame.display.update()
