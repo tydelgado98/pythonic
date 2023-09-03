aae7ca49e5a010a7ca78ffe864722a54bcfa4c45
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-03 16:48:41 -0400

    making a pygame

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Pythonic")
+
+img = pygame.image.load('picc.jpg')
+pygame.display.set_icon(img)
+
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
