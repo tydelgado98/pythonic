a47c9ec8ac47667d30eec46696dcfe5167fab391
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-03 16:45:30 -0400

    making a pygame

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-
+pygame.display.set_caption("Pythonic")
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
