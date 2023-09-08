c1aedf066767a22cbf3367a48c70d1ebdf55f484
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-07 20:27:35 -0400

    making the framerate

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ pygame.init()
 
 screen = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Pythonic")
+clock = pygame.time.Clock()
 
 img = pygame.image.load('./assets/picc.jpg')
 pygame.display.set_icon(img)
@@ -15,3 +16,4 @@ while True:
             pygame.quit()
             quit()
     pygame.display.update()
+    clock.tick(60)
