6821b70eda4cb6125061ba3895c45271e762a011
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-09 23:53:36 -0400

    making a new background

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ clock = pygame.time.Clock()
 img = pygame.image.load('./assets/picc.jpg')
 pygame.display.set_icon(img)
 test_font = pygame.font.Font('none', 40)
+text_surface = test_font.render('My Game', False, 'red')
 
 
 # test_surface = pygame.image.load('./assets/Sky.png')
