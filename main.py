d89dbbbf6487b95b55ae07cd02c0af7b46643e63
Justin Delgado <delgadojustin100@yahoo.com>
2023-09-18 20:19:54 -0400

    adding clarification to the code

--- main.py
+++ main.py
@@ -8,14 +8,19 @@ clock = pygame.time.Clock()
 
 img = pygame.image.load('./assets/picc.jpg')
 pygame.display.set_icon(img)
+
+# Font
 test_font = pygame.font.Font('font/pixeltype.ttf' , 40)
 text_surface = test_font.render('Pythonic', False, 'blue')
 
-
+# Images
 test_surface = pygame.image.load('./assets/Sky.png')
 test_ground = pygame.image.load('./assets/ground.png')
 snail_surface = pygame.image.load('./assets/snail1.png')
+
+
 snail_x_pos = 800
+
 
 while True:
     for event in pygame.event.get():
@@ -27,13 +32,14 @@ while True:
     screen.blit(test_surface, (0, 0))
     screen.blit(test_ground, (0, 300))
     screen.blit(text_surface, (350, 50))
+     # that means that the snail will move 3 pixels to the left
     snail_x_pos -= 3 
-    # that means that the snail will move 3 pixels to the left
     if snail_x_pos < -100 :
+     # that means that the snail will move to the right side of the screen
         snail_x_pos = 800
- # that means that the snail will move to the right side of the screen
+     # the 264 is the y position of the snail
     screen.blit(snail_surface, (snail_x_pos, 264))
-    # the 264 is the y position of the snail
+   
           
 
     pygame.display.update()
